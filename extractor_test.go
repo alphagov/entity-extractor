871ea9d1215bfa9efde5b6dc7aff551605cd35df
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-01-13 17:55:57 +0000

    Acceptance test for LoadEntities method
    this loads a fixture file from the filesystem and then verifies the
    data structure which has been created.

--- extractor_test.go
+++ extractor_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"path"
+	"runtime"
 	"testing"
 )
 
@@ -35,5 +37,40 @@ func TestParseEntityFromJson(t *testing.T) {
 		assert.Equal(t, example.expectedError, err, fmt.Sprint("unexpected error in example ", i))
 		assert.Equal(t, example.expectedTerms, actual.Terms, fmt.Sprint("terms differ in example ", i))
 		assert.Equal(t, example.expectedId, actual.Id, fmt.Sprint("ids differ in example ", i))
+	}
+}
+
+func fixturePath(fixtureFile string) string {
+	_, filename, _, _ := runtime.Caller(1)
+	return path.Join(path.Dir(filename), "data", fixtureFile)
+}
+
+func TestLoadEntities(t *testing.T) {
+	config := Config{
+		extractAddress: ":9999",
+		entitiesPath:   fixturePath("entities.jsonl"),
+		logPath:        "STDERR",
+	}
+
+	extractor := NewExtractor(&config)
+	extractor.LoadEntities()
+
+	expectedTerms := [...]string{
+		"Government digital service",
+		"GDS",
+		"Ministry of Justice",
+		"MoJ",
+	}
+	for i, expectedTerm := range expectedTerms {
+		assert.Equal(t, expectedTerm, extractor.entities.terms[i])
+	}
+	expectedIds := [...]string{
+		"1",
+		"1",
+		"2",
+		"2",
+	}
+	for i, expectedId := range expectedIds {
+		assert.Equal(t, expectedId, extractor.entities.termOffsetToEntity[i].id)
 	}
 }
